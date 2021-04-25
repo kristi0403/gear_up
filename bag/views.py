5b794bbdcf9792fc1c72b26cd040e3d232dd1428
kristi0403 <kristirapi@hotmail.it>
2021-04-25 16:31:44 +0000

    added the functionality to get the quantm the form

--- bag/views.py
+++ bag/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 def view_bag(request):
     """ A view that renders the bag contents page """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
