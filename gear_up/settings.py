2c68e2d7b4afa80b8deb0ff8f58aac9cd69ed537
kristi0403 <kristirapi@hotmail.it>
2021-04-30 02:07:57 +0000

    added stripe to the project level settings

--- gear_up/settings.py
+++ gear_up/settings.py
@@ -173,3 +173,11 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+SRANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'GBP'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
