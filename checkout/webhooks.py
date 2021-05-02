a1b7adb8163bf24173efb2a4c8ff40762fcffde3
kristi0403 <kristirapi@hotmail.it>
2021-05-02 15:25:18 +0000

    added class methods to webhook handler and webhook view

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
-
 from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
@@ -14,7 +13,7 @@ import stripe
 def webhook(request):
     """Listen for webhooks from Stripe"""
     wh_secret = settings.STRIPE_WH_SECRET
-    stripe.api.key = settings.STRIPE_SECRET_KEY
+    stripe.api_key = settings.STRIPE_SECRET_KEY
 
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
@@ -31,3 +30,23 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(contact=e, status=400)
+
+    # Set up webhook handler
+    handler = StripeWH_Handler(request)
+
+    # Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    # Get the webhook type from Stripe
+    event_type = event['type']
+
+    # If there's a handler for it, get it from the event map
+    # Use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the event handler with the event
+    response = event_handler(event)
+    return response
