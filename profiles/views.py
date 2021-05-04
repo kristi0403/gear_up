664336c115005589bc3ddd86c89b2e7e4c9ed89c
kristi0403 <kristirapi@hotmail.it>
2021-05-04 10:49:41 +0000

    fixed profiler to profile

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
-from .forms import UserProfilerForm
+from .forms import UserProfileForm
 
 from checkout.models import Order
 
@@ -12,12 +12,12 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == "POST":
-        form = UserProfilerForm(request.POST, instance=profile)
+        form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
 
-    form = UserProfilerForm(instance=profile)
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
