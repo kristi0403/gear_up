6f574632813252550437ca2853e447718dd8cd57
kristi0403 <kristirapi@hotmail.it>
2021-05-11 04:09:25 +0000

    created the sub category class and the Special category class

--- products/models.py
+++ products/models.py
@@ -16,6 +16,36 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Special_category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Special_categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Sub_category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Sub_categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Platform(models.Model):
 
     class Meta:
@@ -32,6 +62,10 @@ class Platform(models.Model):
 
 
 class Product(models.Model):
+    sub_category = models.ForeignKey(
+        'Sub_category', null=True, blank=True, on_delete=models.SET_NULL)
+    special_category = models.ForeignKey(
+        'Special_category', null=True, blank=True, on_delete=models.SET_NULL)
     platform = models.ForeignKey(
         'Platform', null=True, blank=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(
