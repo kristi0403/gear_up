81a1509b33831382deff0f49a9697a10309b7aaf
kristi0403 <kristirapi@hotmail.it>
2021-05-17 18:10:15 +0000

    made the automatic deployment from heroku to git

--- gear_up/settings.py
+++ gear_up/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y&hr6m7f=&ja!l@20y4)d!_48vbj!7gy)4^8$en8h!f7zpf3(r'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['this-is-gear-up.herokuapp.com', 'localhost']
 
